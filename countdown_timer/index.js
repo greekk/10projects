d77420a67f3cb7445a968f8a723e262172c8656c
greekk <greekks@gmail.com>
2022-08-03 17:12:40 +0600

    second commit. layout projects

--- countdown_timer/index.js
+++ countdown_timer/index.js
@@ -1,7 +1,16 @@
-const newYears =  `1st Jan 2022`;
+const newYears =  '1 Jan 2023';
 
 function coundown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
-    console.log(newYearsDate - currentDate);
+    const totalSeconds = Math.floor((newYearsDate - currentDate) /  1000);
+    const days = Math.floor((totalSeconds / 3600) / 24);
+    const hours =  Math.floor(days / 60);
+    const minutes =  0;
+    
+    
+    console.log(`${days} : ${hours} : ${minutes} : ${totalSeconds}`);
 }
+
+coundown();
+setInterval(coundown, 1000);
